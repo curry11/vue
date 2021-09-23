7317f2fa13895645fbcf69839356565f1b23f81b
jzdingwenlong <jzdingwenlong@qq.com>
2021-09-23 16:27:29 +0800

    增加项目介绍

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# vue
+# 项目结构
+  1. node_modules 项目安装的依赖包
+  2. public 存放静态文件
+  3. assets 存放静态文件
+  4. components  存放组件一般是公共组件
+  5. router 路由器
+  6. store 状态管理器 主要用于组件之间的数据共享
+  7. views 存放页面
+  8. APP.vue 项目的入口页面 根组件
+  9. main.js 项目的入口调用APP.vue
+  10. package.json  npm包配置文件
+  11. mock.js 引入mock数据
--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Element from 'element-ui'   //引入elemetnUI
+import "element-ui/lib/theme-chalk/index.css"
+import axios from 'axios'
 
+Vue.use(Element)
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
+require("./mock") //引入mock数据，关闭则注释该行
 
 new Vue({
   router,
