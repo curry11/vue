70add0a4fdda894e3e240ae05358856925a29f13
jzdingwenlong <jzdingwenlong@qq.com>
2021-10-23 18:32:03 +0800

    提交代码

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,19 +3,76 @@
     <el-row type="flex" class="row-bg" justify="center">
       <el-col :span="6">
         <h2>欢迎来到VueAdmin管理系统</h2>
+        <el-image :src="require('@/assets/MarkerHub.jpg')"></el-image>
+        <p>你好</p>
+        <p>加油</p>
       </el-col>
-      <el-col :span="6"
-        ><div class="grid-content bg-purple-light"></div
-      ></el-col>
-      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="1">
+        <el-divider direction="vertical"></el-divider>
+      </el-col>
+      <el-col :span="6">
+        <el-form
+          :model="ruleForm"
+          :rules="rules"
+          ref="ruleForm"
+          label-width="100px"
+          class="demo-ruleForm"
+        >
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="ruleForm.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input v-model="ruleForm.password"></el-input>
+          </el-form-item>
+          <el-form-item label="验证码" prop="code">
+            <el-input v-model="ruleForm.code"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm')"
+              >立即创建</el-button
+            >
+            <el-button @click="resetForm('ruleForm')">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+
+  data() {
+    return {
+      ruleForm: {
+        name: ''
+      },
+      rules: {
+        name: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
 }
+
 </script>
 
 <style scoped>
